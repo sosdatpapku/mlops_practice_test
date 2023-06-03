a349bdabbfd88d545167ac31b0628a75f3878911
sosdatpapku <114177169+sosdatpapku@users.noreply.github.com>
2023-06-03 16:10:45 +0400

    Update pipeline.sh

--- lab1/pipeline.sh
+++ lab1/pipeline.sh
@@ -2,8 +2,13 @@
 pip3 install scikit-learn
 pip3 install pandas
 
-
+echo "----Create Dataset (begin)-----"
 python3 data_creation.py
+echo "----Create Dataset (end)-----"
+echo "----Processing Dataset (begin)-----"
 python3 data_preprocessing.py
+echo "----Processing Dataset (end)-----"
+echo "----Use the Model for Preparation-----"
 python3 model_preparation.py
+echo "----Use the Model for Testing-----"
 python3 model_testing.py
